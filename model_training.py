c2b155a85b1731090d6776ce9cef0b0d132f2fe3
mohamedashik79 <mohamedashik2k25@gmail.com>
2025-05-07 14:19:19 +0530

    model_training.py

--- model_training.py
+++ model_training.py
@@ -1,13 +1,13 @@
+from data_preprocessing import load_and_clean_data
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
-from data_preprocessing import load_and_clean_data
 import pandas as pd
 
-df = load_and_clean_data('../data/sample_accident_data.csv')
+df = load_and_clean_data('sample_accident_data.csv')  # updated path
 X = df.drop('Severity', axis=1)
 y = df['Severity']
 
-# Dummy encoding for simplicity
+# Dummy encoding
 X = pd.get_dummies(X)
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
